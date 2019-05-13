58494248e05cb4f150fb085128c40e603653af92
rafaelmatheus <rafaelmatheusdecastro@hotmail.com.br>
2019-05-13 19:45:56 -0300

    Alterações

--- src/test/java/br/com/topicos/steps/Steps.java
+++ src/test/java/br/com/topicos/steps/Steps.java
@@ -1,5 +1,7 @@
 package br.com.topicos.steps;
 
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -23,6 +25,7 @@ public class Steps {
 	}
 	
 	@After
+	public void 
 	@Given("^Que o usuario efetue o login no sistema$")
 	public void efetueLoginNoSistema(){
 	
